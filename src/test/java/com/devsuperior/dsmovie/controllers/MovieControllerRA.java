ae5e68e53e01e3338a1e951a7c7ffba83486add6
icaro-freitas <icaro_freitas2014@outlook.com>
2024-10-14 16:38:15 -0300

    Tests added for movies endpoint findAll method

--- src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -1,16 +1,37 @@
 package com.devsuperior.dsmovie.controllers;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
+
 import org.json.JSONException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MovieControllerRA {
 	
+	private String movieTitle;
+	
+	
+	@BeforeEach
+	public void setUp() {
+		movieTitle = "Rogue";
+	}
+	
 	@Test
 	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
+		given().get("/movies?page=0").then().statusCode(200).body("content.title",
+				hasItems("The Witcher", "Venom: Tempo de Carnificina"));
 	}
 	
 	@Test
 	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {		
+		given().get("/movies?title={movieTitle}", movieTitle).then().statusCode(200).body("content.id[0]", is(10))
+		.body("content.title[0]", equalTo("Rogue One: Uma História Star Wars")).body("content.score[0]", is(0.0F))
+		.body("content.count[0]", is(0))
+		.body("content.image[0]", equalTo(
+				"https://www.themoviedb.org/t/p/w533_and_h300_bestv2/6t8ES1d12OzWyCGxBeDYLHoaDrT.jpg"));
 	}
 	
 	@Test
